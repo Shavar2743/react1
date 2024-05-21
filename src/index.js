cc12e8b0d9b3bc960f1d29e9b9fdcb8a80118b20
Shavar2743 <markus.shavar@web.de>
2024-05-21 23:45:25 +0200

    hausi

--- src/index.js
+++ src/index.js
@@ -4,14 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function addieren(z1,z2){
+  return z1 +z2;
+}
+
+function wurzel(w1){
+  return Math.sqrt(w1);
+}
+
+function datumadd() {
+let datum = new Date().toLocaleDateString();
+return datum;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<h1 className="h1">React ist cool</h1>
+
+<button className="button1">Dieser Button macht nichts</button>
+
+<h2 style={{color: "green", fontSize: 35}}>Die Geschichte der Wikinger</h2>
+
+<p style={{color: "blue"}}>Die Geschichte der Wikinger begann in den kalten, rauen Landschaften Skandinaviens. Mutige Seefahrer stachen in ihren Langschiffen in See, um ferne Länder zu erkunden. Sie waren nicht nur gefürchtete Krieger, sondern auch geschickte Händler und Entdecker. Ihre Reisen führten sie nach England, Island, Grönland und sogar bis nach Nordamerika. In ihren Siedlungen lebten sie nach strengen Gesetzen und verehrten mächtige Götter wie Odin und Thor. Die Wikinger brachten mit ihren Eroberungen und Entdeckungen große Veränderungen nach Europa. Ihre Runenschriften erzählen noch heute von ihren Heldentaten und Abenteuern. Doch mit der Zeit ließen sie sich nieder und viele wurden zu Christen. Die Wikingerzeit endete im 11. Jahrhundert, aber ihr Erbe lebt weiter. Ihre Legenden und Geschichten faszinieren die Menschen bis heute.
+</p>
+
+<p style={{color: "red"}}>Deine Glückszahl: {Math.random() *100};</p>
+
+<p>Heute ist der: {datumadd()};</p>
+</>
+
+
+);
